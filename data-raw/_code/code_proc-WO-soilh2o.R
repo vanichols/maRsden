3eaa756bacc33387d6f83a7a938d022919ba1e0d
vanichols <vnichols@iastate.edu>
2019-12-16 09:06:24 -0600

    add soilh2o and wea data

---
+++ data-raw/_code/code_proc-WO-soilh2o.R
@@ -0,0 +1,60 @@
+#############################
+##
+## Dec 16 2019
+## Process Will's data
+## Writes file to _tidy and put in package
+##
+##############################
+
+rm(list=ls())
+library(tidyverse)
+library(lubridate)
+library(janitor)
+
+
+# h2o data ----------------------------------------------------------------
+
+
+swr <- read_csv("data-raw/_raw/wo/raw_soilH2O13-14.csv", skip = 1)
+plotkey <- read_csv("data-raw/_tidy/plotkey.csv")
+
+
+# wrangle -----------------------------------------------------------------
+
+sw <-
+  swr  %>%
+  #--fix date
+  mutate(date = as.character(date),
+         lubedate = mdy(date),
+         year = year(lubedate),
+         doy = yday(lubedate)) %>%
+  select(year, doy, everything(), -lubedate, -date) %>%
+  #--gather depths
+  gather(`0-10cm_soilH2O_g.g`:`0-30cm_soilH2O_g.g`, key = depth, value = soilh2o_g.g) %>%
+  mutate(depth_cm = str_remove(depth, "cm_soilH2O_g.g")) %>%
+  #--fix plot/block things
+  mutate(block = paste0("b", block)) %>%
+  left_join(plotkey) %>%
+  #--clean up
+  select(year, doy, plot_id, depth_cm, soilh2o_g.g)
+
+
+
+# viz ---------------------------------------------------------------------
+
+sw %>%
+  ggplot(aes(doy, soilh2o_g.g)) +
+  geom_line(aes(color = plot_id)) +
+  facet_grid(year ~ depth_cm)
+
+
+# write it ----------------------------------------------------------------
+
+# to tidy folder (just for reference?)
+
+sw %>% write_csv("data-raw/_tidy/soilh2o_wo.csv")
+
+# name it what I want the actual dtaa call to be
+mrs_soilh2o_wo <- sw
+
+usethis::use_data(mrs_soilh2o_wo, overwrite = T)
---
+++ data-raw/_code/code_proc-wea.R
@@ -0,0 +1,76 @@
+#############################
+##
+## Dec 16 2019
+## Process Iowa Mesonet files
+## Writes file to _tidy and put in package
+##
+##############################
+
+rm(list=ls())
+library(tidyverse)
+library(lubridate)
+
+
+# h2o data ----------------------------------------------------------------
+
+
+wear <- read_csv("data-raw/_raw/wea/raw_wthr1987-2017_im.csv")
+wea18r <- read_csv("data-raw/_raw/wea/raw_wthr2018_im.csv")
+
+
+# wrangle -----------------------------------------------------------------
+
+# wea 'day' is a character, but wea18 day is a date, so must handle separately
+wea <-
+  wear %>%
+  mutate(date = as.character(day),
+         lubedate = mdy(date),
+         year = year(lubedate),
+         doy = yday(lubedate)) %>%
+  select(year, doy, everything(), -lubedate, -day, -date)
+
+wea18 <-
+  wea18r %>%
+  mutate(lubedate = ymd(day),
+         year = year(lubedate),
+         doy = yday(lubedate)) %>%
+  select(year, doy, everything(), -lubedate, -day)
+
+# bind weather years tog here
+dat <-
+  bind_rows(wea, wea18) %>%
+  arrange(year, doy) %>%
+  rename("hiT_c" = highc,
+         "loT_c" = lowc,
+         "precip_mm" = precipmm,
+         "narr_srad_mjday" = narr_srad) %>%
+  select(station, station_name, year, doy, everything())
+
+# viz check ---------------------------------------------------------------------
+
+dat %>%
+  ggplot(aes(doy, narr_srad_mjday)) +
+  geom_line(aes(color = year))
+
+
+# write it ----------------------------------------------------------------
+
+# make a 2008-present dataset, and a historical one
+# to tidy folder (just for reference?)
+
+#--weather 2008-present
+mrs_wea_08_pres <-
+  dat %>%
+  filter(year > 2007)
+
+mrs_wea_08_pres %>% write_csv("data-raw/_tidy/wea_08_pres.csv")
+usethis::use_data(mrs_wea_08_pres, overwrite = T)
+
+#--historical weather 1987-present
+mrs_wea_hist <-
+  dat  %>%
+  filter(year > 1986)
+
+mrs_wea_hist %>% write_csv("data-raw/_tidy/wea_hist.csv")
+usethis::use_data(mrs_wea_hist, overwrite = T)
+

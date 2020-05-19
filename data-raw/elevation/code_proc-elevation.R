ccd9ce420067b68c6d6ff798e5255e8fb66359dc
vanichols <vnichols@iastate.edu>
2020-05-19 07:29:38 -0500

    create mrs_elevation data

--- data-raw/elevation/code_proc-elevation.R
+++ data-raw/elevation/code_proc-elevation.R
@@ -1,15 +1,15 @@
 ########################
 #
-# Date of creation: March 13 2020
+# Date of creation: May 19 2020
 #
 # Author: Gina
 #
-# Purpose: Process 2018 and 2019 biomass and associated LAI data
+# Purpose: Process elevation data from Jess/GIS
 #
 # NOTES:
 #
 #
-# Last updated: Feb 18 2020: added date
+# Last updated:
 #
 #########################
 
@@ -23,25 +23,31 @@ library(janitor) #--used to clean data
 pk <- read_csv("data-raw/plotkey/plotkey.csv") %>%
   filter(year == 2018)
 
+elev_raw <- read_excel("data-raw/elevation/raw-Jess/marsden_Table.xls")
 
-# 2018 data ---------------------------------------------------------------
+b1 <- seq(11, 19)
+b2 <- seq(21, 29)
+b3 <- seq(31, 39)
+b4 <- seq(41, 49)
 
-graw <- read_excel("data-raw/earrows/rd_mars-grain-samples-2018.xlsx",
-                   skip = 5,
-                   na = "NA")
+
+mrs_elevation <-
+  elev_raw %>%
+  filter(!(Id %in% c(0, 10, 29))) %>%
+  mutate(plot = c(b1, b2, b3, b4)) %>%
+  clean_names() %>%
+  select(-objectid, -id, -sum, -variety) %>%
+  pivot_longer(min:median) %>%
+  mutate(value = value/100) %>%
+  pivot_wider(names_from = name, values_from = value) %>%
+  rename(mean_elev_m = mean) %>%
+  select(plot, count, area, mean_elev_m, everything())
 
 
-mrs_earrows <-
-  graw %>%
-  mutate(date = ymd(date),
-         year = year(date),
-         doy = yday(date),
-         harv_crop = trt,
-         ear_id = paste0("p", plot, "-", ear_no),
-         rows_nu = row_no) %>%
-  left_join(pk, by = c("plot", "harv_crop", "year")) %>%
-  select(year, date, doy, plot_id, ear_id, rows_nu)
+mrs_elevation %>%
+  ggplot(aes(plot, mean_elev_m)) +
+  geom_point()
 
-mrs_earrows %>%  write_csv("data-raw/earrows/earrows.csv")
+mrs_elevation %>%  write_csv("data-raw/elevation/elevation.csv")
 
-usethis::use_data(mrs_earrows, overwrite = T)
+usethis::use_data(mrs_elevation, overwrite = T)

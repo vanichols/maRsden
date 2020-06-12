155769b392dcadb11ebd6cdd706668edaa84ab6e
vanichols <vnichols@iastate.edu>
2020-06-12 16:07:09 -0500

    fix rootdepth, doesn't include phenologynow

--- data-raw/rootdepth/code_proc-rootdepth.R
+++ data-raw/rootdepth/code_proc-rootdepth.R
@@ -4,6 +4,7 @@
 # last updated: feb 18 2020 (added dates)
 #              2019 planting has too many plots
 #              march 13 2020 (updated pkg structure, fixed phenology)
+#              june 12 2020 (add 2020 data)
 #
 # purpose: Process max root depth data from 2018 and 2019
 #
@@ -18,10 +19,13 @@ library(tidyverse)
 library(lubridate)
 library(readxl) #--used to read Excel files
 library(janitor) #--used to clean data
-#library(fuzzyjoin) #--didn't work for me
+library(maRsden)
+data("mrs_plotkey")
 
-pk <- read_csv("data-raw/plotkey/plotkey.csv")
 
+pk <-
+  mrs_plotkey %>%
+  filter(year > 2017)
 
 # 2018 --------------------------------------------------------------------
 
@@ -35,11 +39,11 @@ rd18 <-
   mutate(year = year(date),
          doy = yday(date),
          block = paste0("b", block)) %>%
-  left_join(pk) %>%
+  left_join(pk %>% filter(year == 2018)) %>%
   ungroup() %>%
   mutate(rootdepth_cm = rootdepth_in * 2.54,
          pl_stage = stage) %>%
-  select(year, date, doy, plot_id, pl_stage, rootdepth_cm)
+  select(date, doy, plot_id, rootdepth_cm)
 
 
 
@@ -69,57 +73,13 @@ rd19raw <-
   mutate(date2 = ifelse(date == as.Date("2019-09-16"), as.Date("2019-09-17"), as.Date(date)),
          date3 = as_date(date2)) %>%
   select(-date, -date2) %>%
-  rename("date" = "date3")
-
-
-#--note this doesn't have a 'planting' data point
-rd19 <- rd19raw %>%
+  rename("date" = "date3") %>%
   mutate(date = as_date(date),
          year = year(date),
          doy = yday(date)) %>%
-  left_join(pk) %>%
-  select(year, date, doy, plot_id, mrd_cm) %>%
+  left_join(pk %>% filter(year == 2019)) %>%
+  select(date, doy, plot_id, mrd_cm) %>%
   rename("rootdepth_cm" = mrd_cm)
-
-
-# have to get phenology to merge with it
-
-phenroots <-
-  read_csv("data-raw/phen/phen.csv") %>%
-  filter(year == 2019) %>%
-  select(year, date, doy, plot_id, pl_stage) %>%
-  mutate(
-    #--replace the one VT with R1
-    pl_stage = ifelse(pl_stage == "VT", "R1", pl_stage),
-    #--wyatt forgot to do 2019_21 on day 213, just make it R1
-    pl_stage = ifelse( (plot_id == "2019_21" & doy == 213), "R1", pl_stage),
-    #--separate into numbers/letters
-    snum = as.numeric(str_sub(pl_stage, 2)),
-    slet = str_sub(pl_stage, 1, 1)) %>%
-  # find average stage for a plot
-  group_by(year, date, doy, plot_id, slet) %>%
-  summarise(snum_mn = mean(snum, na.rm=T),
-            snum_mn = round(snum_mn, 0)) %>%
-  ungroup() %>%
-  mutate(pl_stage = paste0(slet, snum_mn)) %>%
-  left_join(pk) %>%
-  select(year, date, doy, plot_id, pl_stage) %>%
-  # make dummy doy2 to merge w/roots
-  mutate(doy_tmp = case_when(
-      doy == 190 ~ 189,
-      doy == 205 ~ 203,
-      doy == 213 ~ 212,
-      doy == 232 ~ 231,
-      TRUE ~ doy)) %>%
-  select(-doy, -date)
-
-#--combine root data w/phen
-rdphen19 <-
-  rd19 %>%
-  left_join(phenroots, by = c("doy" = "doy_tmp",
-                              "year", "plot_id")) %>%
-  #--no phen 9/17, say R6
-  mutate(pl_stage = ifelse(doy == 260, "R6", pl_stage))
 
 #--make a dummy planting (6/3/2020) data point w/rootdepth = 0
 plant19 <-
@@ -128,20 +88,61 @@ plant19 <-
   select(plot_id) %>%
   unique() %>%
   mutate(date = ymd("2019-06-03"),
-         year = year(date),
          doy = yday(date),
-         pl_stage = "planting",
          rootdepth_cm = 0)
 
-rd19phen2 <-
-  rdphen19 %>%
+rd19 <-
+  rd19raw %>%
   bind_rows(plant19)
 
-# combine 2018 and 2019 data ----------------------------------------------
+
+
+# 2020 data ---------------------------------------------------------------
+
+#--corn was planted 4/23/2020
+plant20 <-
+  pk %>%
+  filter(year == 2020, rot_trt %in% c("C4", "C2")) %>%
+  select(plot_id) %>%
+  unique() %>%
+  mutate(date = ymd("2020-04-23"),
+         doy = yday(date),
+         rootdepth_cm = 0)
+
+
+rd20raw <-
+  tibble(files = list.files(myrootdir)) %>%
+  mutate(path = paste0(myrootdir, files)) %>%
+  # keep only maxrootdepth ones
+  filter(grepl('maxrootdepth', files)) %>%
+  filter(grepl('2020', files)) %>%
+  filter(!grepl("20200522", files)) %>% #--this first day was a bust
+  # make sure I don't get .txt files by mistake
+  filter(grepl('.xlsx', files)) %>%
+  # read each file
+  mutate(data = path %>% map(read_excel, skip = 5)) %>%
+  select(data) %>%
+  unnest(cols = c(data)) %>%
+  fill(date, plot) %>%
+  mutate(mrd_cm = ifelse(is.na(maxrootdepth_cm), maxrootdepth_in*2.54, maxrootdepth_cm)) %>%
+  group_by(date, plot) %>%
+  summarise(mrd_cm = mean(mrd_cm, na.rm = T)) %>%
+  ungroup() %>%
+  mutate(date = as_date(date),
+         doy = yday(date)) %>%
+  left_join(pk %>% filter(year == 2020)) %>%
+  select(date, doy, plot_id, mrd_cm) %>%
+  rename("rootdepth_cm" = mrd_cm)
+
+rd20 <-
+  rd20raw %>%
+  bind_rows(plant20) %>%
+  arrange(date, plot_id)
+
+# combine 2018, 2019, 2020 data ----------------------------------------------
 
 mrs_rootdepth <-
-  bind_rows(rd18, rd19phen2) %>%
-  arrange(year, date, doy, plot_id)
+  bind_rows(rd18, rd19, rd20) %>%
+  arrange(date, plot_id)
 
-mrs_rootdepth %>% write_csv("data-raw/rootdepth/rootdepth.csv")
 usethis::use_data(mrs_rootdepth, overwrite = T)

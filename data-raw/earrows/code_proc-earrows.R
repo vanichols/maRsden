c16bf97a157c5e412a4cd95c9f962f6c7f7bd015
vanichols <vnichols@iastate.edu>
2020-10-02 16:29:02 -0500

    add 2020 earrows data

--- data-raw/earrows/code_proc-earrows.R
+++ data-raw/earrows/code_proc-earrows.R
@@ -21,18 +21,18 @@ library(readxl) #--used to read Excel files
 library(janitor) #--used to clean data
 
 pk <- read_csv("data-raw/plotkey/plotkey.csv") %>%
-  filter(year == 2018)
+  filter(year %in% c(2018, 2020))
 
 
 # 2018 data ---------------------------------------------------------------
 
-graw <- read_excel("data-raw/earrows/rd_mars-grain-samples-2018.xlsx",
+graw18 <- read_excel("data-raw/earrows/rd_mars-grain-samples-2018.xlsx",
                    skip = 5,
                    na = "NA")
 
 
-mrs_earrows <-
-  graw %>%
+mrs_earrows18 <-
+  graw18 %>%
   mutate(date = ymd(date),
          year = year(date),
          doy = yday(date),
@@ -41,6 +41,43 @@ mrs_earrows <-
          rows_nu = row_no) %>%
   left_join(pk, by = c("plot", "harv_crop", "year")) %>%
   select(year, date, doy, plot_id, ear_id, rows_nu)
+
+
+# 2020 data ---------------------------------------------------------------
+
+graw20 <- read_excel("data-raw/earrows/rd_mars-grain-samples-2020.xlsx",
+                   skip = 5,
+                   na = "NA")
+
+mrs_earrows20 <-
+  graw20 %>%
+  fill(date, plot) %>%
+  mutate(date = ymd(date),
+         year = year(date),
+         doy = yday(date),
+         ear_id = paste0("p", plot, "-", ear_no),
+         rows_nu = row_no,
+         plot_id = paste(year, plot, sep = "_")) %>%
+  select(year, date, doy, plot_id, ear_id, rows_nu)
+
+
+
+# bind together -----------------------------------------------------------
+
+mrs_earrows <-
+  mrs_earrows18 %>%
+  bind_rows(mrs_earrows20)
+
+library(beeswarm)
+library(ggbeeswarm)
+mrs_earrows %>%
+  left_join(pk) %>%
+  separate(plot_id, into = c("year", "plot")) %>%
+  ggplot(aes(rot_trt, rows_nu)) +
+  geom_jitter(aes(color = block, shape = rot_trt), size = 5) +
+  facet_grid(.~year)
+
+
 
 mrs_earrows %>%  write_csv("data-raw/earrows/earrows.csv")
 

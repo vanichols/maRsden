6db20306f3b17bf8f0f7897774361a50ef791b6d
vanichols <vnichols@iastate.edu>
2020-10-06 15:33:19 -0500

    fix corn yield data, only 2018-2020, don't have raw data for other years

--- data-raw/yields/code_proc-yields.R
+++ data-raw/yields/code_proc-yields.R
@@ -1,11 +1,11 @@
 #########################
-# created: sept 17 2020
+# created: sept 17 2019
 #
 # purpose: process corn yields
 #
-# NOTES:
+# NOTES: final data is in DRY Mg/ha
 #
-# last updated:
+# last updated: 10/6/2020 (adding 2020 data, cleaning things up)
 #
 #########################
 
@@ -24,12 +24,48 @@ library(janitor) #--used to clean data
 
 pk <- read_csv("data-raw/plotkey/plotkey.csv")
 
+#--has it's own format, I don't want to tuse this as I don't currently have the raw data...
+#d17 <- read_csv("data-raw/yields/raw/rd_cornyld_2003-17.csv", skip = 4) %>%
+#  arrange(year) %>%
+
+
+d18 <-
+  read_csv("data-raw/yields/raw/rd_cornyld-2018.csv", skip = 4) %>%
+  mutate(trt = gsub("[[:punct:]]+", "", trt)) %>%
+  rename(harv_crop = rot) %>%
+  group_by(plot, harv_crop) %>%
+  summarise(yield_Mgha = (1-0.155) * mean(yield_Mgha155, na.rm = T)) %>%
+  mutate(year = 2018)
+
+
+d19 <-
+  read_csv("data-raw/yields/raw/rd_cornyld-2019.csv", skip = 4) %>%
+  fill(plot, rot) %>%
+  mutate(trt = gsub("[[:punct:]]+", "", trt)) %>%
+  rename(harv_crop = rot) %>%
+  group_by(plot, harv_crop) %>%
+  summarise(yield_Mgha = (1-0.155) * mean(yield_Mgha155, na.rm = T)) %>%
+  mutate(year = 2019)
+
+
+d20 <-
+  read_csv("data-raw/yields/raw/rd_cornyld-2020.csv", skip = 5) %>%
+  mutate(trt = str_to_lower(trt)) %>%
+  rename(harv_crop = rot) %>%
+  group_by(plot, harv_crop) %>%
+  summarise(yield_Mgha = (1-0.155) * mean(yield_Mgha155, na.rm = T)) %>%
+  mutate(year = 2020)
+
+
+
+
 mrs_cornylds <-
-  read_csv("data-raw/yields/raw/td_corn-yields.csv") %>%
-  mutate(block = paste0("b", block)) %>%
-  rename("harv_crop" = "trt") %>%
+  d18 %>%
+  bind_rows(d19) %>%
+  bind_rows(d20) %>%
   left_join(pk) %>%
-  arrange(year)
+  ungroup() %>%
+  select(plot_id, yield_Mgha)
 
 
 mrs_cornylds %>%  write_csv("data-raw/yields/mrs_cornylds.csv")
